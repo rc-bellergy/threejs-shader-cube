cc9b6d4471887b1f257d3f2f2453851d48ea119d
Bellergy RC <rc@bellergy.com>
2021-03-11 17:26:07 +0800

    change the u_time position

--- src/shaders/simple4/fragment.glsl
+++ src/shaders/simple4/fragment.glsl
@@ -9,7 +9,7 @@ uniform float u_spacing;
 varying vec2 vUv;
 
 float rand(vec2 co) {
-    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453123 + u_time);
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * (43758.5453123 + u_time));
 }
 void main()
 {
